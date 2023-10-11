4d7aa35ef39ba326bd58601a349922fd163aea1a
Dequavis-Fitzgerald-III <clarkehines@icloud.com>
2023-10-11 17:31:15 +0100

    Added more detail to the Database_builder file, Added in the link tables and worked on getting the references and foreign IDs Working. Need to decide on all the fields that my tables need

--- Database_builder.py
+++ Database_builder.py
@@ -7,7 +7,7 @@ class Database:
     
     def create_users_table(self) -> None:
         self.c.execute('''CREATE TABLE IF NOT EXISTS Users(
-                UserID INT NOT NULL PRIMARY KEY,
+                UserID INT NOT NULL PRIMARY KEY AUTOINCREMENT,
                 Username TEXT NOT NULL,
                 Password TEXT NOT NULL,
                 Email Text NOT NULL
@@ -17,7 +17,7 @@ class Database:
     
     def create_recipes_table(self) -> None:
         self.c.execute('''CREATE TABLE IF NOT EXISTS Recipes(
-                RecipeID INT NOT NULL PRIMARY KEY,
+                RecipeID INT NOT NULL PRIMARY KEY AUTOINCREMENT,
                 Name TEXT NOT NULL,
                 Calories INT NOT NULL
             )
@@ -26,7 +26,28 @@ class Database:
     
     def create_pantry_table(self) -> None:
         self.c.execute('''CREATE TABLE IF NOT EXISTS Pantry(
+                IngredientID INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
                 
             )
         ''')
+        self.conn.commit()
+    
+    def create_User_Recipes_table(self) -> None:
+        self.c.execute('''CREATE TABLE IF NOT EXISTS User-Recipes(
+                FOREIGN KEY(UserID) REFERENCES Users(UserID)
+                FOREIGN KEY(RecipeID) REFERENCES Recipes(RecipeID)
+                PRIMARY KEY (UserID, RecipeID)
+            )   
+        ''')
+        self.conn.commit()
+        
+    def create_User_Pantry_table(self) -> None:
+        self.c.execute('''CREATE TABLE IF NOT EXISTS User-Pantry(
+                FOREIGN KEY(UserID) REFERENCES Users(UserID)
+                FOREIGN KEY(IngredientID) REFERENCES Pantry(IngredientID)
+                PRIMARY KEY (UserID, IngredientID)
+            )   
+        ''')
+        self.conn.commit()
         
