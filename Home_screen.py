4e76241452e8f9bd8c4e46753896be54098675f8
Dequavis-Fitzgerald-III <clarkehines@icloud.com>
2023-11-10 17:55:46 +0000

    started visualizing the responses from the API calls. This at the moment returnst the image of the recipe along with the steps it takes to make the recipe. Improved the home screen by making it scalable as well as having more icons, pictures and funcitonal buttons. Next i am going to make the random recipes part of the home screen.

--- Home_screen.py
+++ Home_screen.py
@@ -1,38 +1,57 @@
-from tkinter import Tk, Label, Frame, Button, Entry, W, PhotoImage
+from tkinter import Tk, Label, Frame, Button, Entry, W, PhotoImage, CENTER
 from tkmacosx import Button as BUTTON
-from Recipe_Finder import RecipeFinder, API
+from Recipe_Finder import RecipeFinder
+from PIL import ImageTk, Image
+
 
 class HomePage:
     """Creates a GUI system for the Foodie Findz APP (prototype)"""
     def __init__(self) -> None:
         self.root = Tk()
         self.root.title('Home Page')
-        self.root.geometry('400x650')
+        self.SCREENWIDTH: int = 400
+        self.SCREENHEIGHT: int = 600
+        self.root.geometry(f'{self.SCREENWIDTH}x{self.SCREENHEIGHT}')
         self.root.config(bg='light blue')
         self.font = ('Times New Roman', 14)
-        self.home_pic = PhotoImage(file='VisualAssets/home_picture.png')
+        self.home_pic = ImageTk.PhotoImage(Image.open('VisualAssets/home_picture.png').resize((self.SCREENWIDTH,int(5 * self.SCREENHEIGHT / 13))))
+        self.menu_icon = ImageTk.PhotoImage(Image.open('VisualAssets/menu_icon.png').resize((int(self.SCREENWIDTH / 8),int(self.SCREENWIDTH / 8))))
+        self.search_icon = ImageTk.PhotoImage(Image.open('VisualAssets/search_icon.png').resize((int(self.SCREENWIDTH / 8),int(self.SCREENWIDTH / 8))))
         self.home_page_creation()
         
     def home_page_creation(self) -> None:
+            #Frames:
             home_frame = Frame(self.root, bd=2, bg='light blue', padx=0, pady=0)
+            home_frame.place(x=-2.5, y=-2.5, width=self.SCREENWIDTH, height=(5 * self.SCREENHEIGHT /13))
+            
+            accounts_frame = Frame(self.root, bd=2, bg='light blue', padx=0, pady=0)
+            accounts_frame.place(x=0, y=(5 * self.SCREENHEIGHT / 13), width=(self.SCREENWIDTH / 3), height=(self.SCREENHEIGHT /13))
+            
+            recipe_frame = Frame(self.root, bd=2, bg='light blue', padx=0, pady=0)
+            recipe_frame.place(x=(self.SCREENWIDTH / 3), y=(5 * self.SCREENHEIGHT / 13), width=(self.SCREENWIDTH / 3), height=(self.SCREENHEIGHT /13))
+            
+            unknown_frame = Frame(self.root, bd=2, bg='light blue', padx=0, pady=0)
+            unknown_frame.place(x=(2 * self.SCREENWIDTH / 3), y=(5 * self.SCREENHEIGHT / 13), width=(self.SCREENWIDTH / 3), height=(self.SCREENHEIGHT /13))
+            
             #pictures:
             Label(home_frame, image=self.home_pic).grid(padx=0, pady=0)
             Label(home_frame, text="Foodie Findz", font=self.font, bg='purple').grid(padx=100, pady=112.5)
-            home_frame.place(x=-2.5, y=-2.5, width=400, height=225)
-            
+    
             # buttons:
-            menu_btn = BUTTON(self.root, text='menu', font=self.font, bg="Turquoise", command=self.menu)
-            menu_btn.place(x=0, y=0, width=50, height=50)
-
-            search_btn = BUTTON(self.root, text='search', font=self.font, bg="Turquoise", command=self.search)
-            search_btn.place(x=350, y=0, width=50, height=50)
+            menu_btn = Button(self.root, image=self.menu_icon, command=self.menu)
+            menu_btn.place(x=0, y=0, width=(self.SCREENWIDTH / 8), height=(self.SCREENWIDTH / 8))
             
-            acounts_btn = BUTTON(self.root, text='accounts', font=self.font, bg="Turquoise", command=self.account)
-            acounts_btn.place(x=0, y=0, width=50, height=50)
+            search_btn = BUTTON(self.root, image=self.search_icon, command=self.search)
+            search_btn.place(x=(self.SCREENWIDTH - self.SCREENWIDTH / 8), y=0, width=(self.SCREENWIDTH / 8), height=(self.SCREENWIDTH / 8))
             
-            recipe_btn = BUTTON(self.root, text='recipe', font=self.font, bg="Turquoise", command=self.recipe)
-            recipe_btn.place(x=225, y=300, width=50, height=50)
+            acounts_btn = BUTTON(accounts_frame, text='accounts', font=self.font, bg="Turquoise", command=self.account)
+            acounts_btn.place(relx=0.5, rely=0.5, anchor=CENTER)
+            
+            recipe_btn = BUTTON(recipe_frame, text='recipe', font=self.font, bg="Turquoise", command=self.recipe)
+            recipe_btn.place(relx=0.5, rely=0.5, anchor=CENTER)
         
+            unknown_btn = BUTTON(unknown_frame, text='?', font=self.font, bg="Turquoise", command=self.recipe)
+            unknown_btn.place(relx=0.5, rely=0.5, anchor=CENTER)
             
             self.place_exit_btn()      
     
--- Recipe_Finder.py
+++ Recipe_Finder.py
@@ -1,8 +1,10 @@
-from tkinter import Tk, Label, Frame, Button, Entry, W, PhotoImage
+from tkinter import Tk, Label, Frame, Button, Entry, W, PhotoImage, S, CENTER
 from tkmacosx import Button as BUTTON
 from requests import get
 from pprint import pprint
-from json import dumps
+from json import dumps, load
+from PIL import ImageTk, Image
+from io import BytesIO
 
 class RecipeFinder:
     """Creates a GUI that connects to a Spoonacular API to recieve recipes and information."""
@@ -26,7 +28,19 @@ class RecipeFinder:
             enter_btn = BUTTON(search_frame, text='search', bg="Turquoise", font=self.font, command=self.API.query_search)
             enter_btn.grid(row=3, column=1, pady=10, padx=10)
             search_frame.place(x=0, y=150, width=500, height=225)
-            self.place_exit_btn()      
+            self.place_exit_btn()   
+    
+    def visualise(self, url, steps_string) -> None:
+        self.clear_root()
+        parsed_image = Image.open(BytesIO(url.content)).resize((400,225))
+        self.image = ImageTk.PhotoImage(parsed_image)
+        image_frame = Frame(self.root, bd=2, bg='light blue', padx=0, pady=0)
+        Label(image_frame, image=self.image).grid(padx=0, pady=0)
+        image_frame.place(x=-2.5, y=-2.5, width=400, height=225)
+        steps_frame = Frame(self.root, bd=2, bg='light blue', padx=0, pady=0)
+        text_label = Label(steps_frame, text=steps_string, font=self.font, wraplength=380, bg='white')
+        text_label.place(relx=0.5, rely=0.5, anchor=CENTER)
+        steps_frame.place(x=0, y=225, width=400, height=425)
         
     def clear_root(self) -> None:
         for widget in self.root.winfo_children():
@@ -35,6 +49,7 @@ class RecipeFinder:
     def place_exit_btn(self) -> None:
         exit_btn = BUTTON(self.root, text='EXIT', font=('Times New Roman', 50), bg="Turquoise", command=self.root.destroy)
         exit_btn.place(x=200, y=600, width=200, height=50)
+    
 
 class API:
     """connects to the Spoonacular API to search for and parse data on recipes."""
@@ -43,26 +58,32 @@ class API:
     
     # searches:
     def query_search(self) -> None:
-        url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={self.API_KEY}&query={recipe_page.query_entry.get()}&cuisine={recipe_page.cuisine_entry.get()}&number=1&includeIngredients={recipe_page.ingredients_entry.get()}"
+        url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={self.API_KEY}&query={recipe_page.query_entry.get()}&cuisine={recipe_page.cuisine_entry.get()}&includeIngredients={recipe_page.ingredients_entry.get()}"
         r = get(url)
         self.recipe_parse(r.json())
     
     def ID_search(self, ID) -> None:
         url = f"https://api.spoonacular.com/recipes/{ID}/information?apiKey={self.API_KEY}"
         r = get(url)
-        self.ID_parse(dumps(r.json()))
+        self.ID_parse(r.json())
         
     # parses:
     def recipe_parse(self, r) -> None:
-        print(type(r))
-        # valuesList = list(r.values())
         recipes = r["results"]
         for i, recipe in enumerate(recipes):
-            if i < 1:
+            if i == 1:
                 self.ID_search(recipe["id"])
     
     def ID_parse(self, r) -> None:
-        pprint(r)
+        print(f"{r['title']}:")
+        steps = r["analyzedInstructions"][0]["steps"]
+        steps_string = ""
+        for i, step in enumerate(steps):
+            steps_string += f"{i+1}){step['step']} \n"
+        print(r['image'])
+        url = get(r['image'])
+        recipe_page.visualise(url, steps_string)
+    
         
 if __name__ == "__main__":
     recipe_page = RecipeFinder()
